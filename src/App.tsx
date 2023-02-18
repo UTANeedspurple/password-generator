d1b8d6d7423c4a58c7b5e1594c50b34f21b79087
UTANeedspurple <125596938+UTANeedspurple@users.noreply.github.com>
2023-02-18 17:39:35 +0000

    the commit

--- src/App.tsx
+++ src/App.tsx
@@ -24,14 +24,14 @@ function App() {
 
   return (
     <div className="flex flex-col max-w-md m-4">
-      <h1 className="text-3xl font-bold text-red-500">Password Generator</h1>
+      <h1 className="text-3xl font-bold text-purple-500">Password Generator</h1>
       <div className="flex flex-row pt-2">
         <button
           onClick={() => {
             setIsBase64(!isBase64);
           }}
           className={`${
-            isBase64 ? "bg-red-300 border-red-300" : "bg-red-500 border-red-500"
+            isBase64 ? "bg-purple-300 border-purple-300" : "bg-purple-500 border-purple-500"
           } flex-1 rounded-lg  p-3 border-2 text-white text-center my-2 mr-2`}
         >
           Password
@@ -42,8 +42,8 @@ function App() {
           }}
           className={`${
             !isBase64
-              ? "bg-red-300 border-red-300"
-              : "bg-red-500 border-red-500"
+              ? "bg-purple-300 border-purple-300"
+              : "bg-purple-500 border-purple-500"
           } flex-1 rounded-lg  p-3 border-2 text-white text-center my-2`}
         >
           Base64
@@ -58,7 +58,7 @@ function App() {
           step={1}
           value={length}
           onChange={(e) => setLength(Number(e.target.value))}
-          className="flex-1 accent-red-500"
+          className="flex-1 accent-purple-500"
         ></input>
       </div>
       <textarea
@@ -66,12 +66,12 @@ function App() {
         value={pass}
         onChange={() => {}}
         rows={5}
-        className="border-red-500 border-solid border-2 p-3 rounded-lg my-2 resize-none"
+        className="border-purple-500 border-solid border-2 p-3 rounded-lg my-2 resize-none"
       />
       <div className="flex flex-row">
         <button
           onClick={generate}
-          className="flex-1 rounded-lg bg-red-500 border-red-500 p-3 border-2 text-white text-center my-2 mr-2"
+          className="flex-1 rounded-lg bg-purple-500 border-purple-500 p-3 border-2 text-white text-center my-2 mr-2"
         >
           Generate
         </button>
@@ -90,7 +90,7 @@ function App() {
               setCopy("Error!");
             }
           }}
-          className="flex-1 rounded-lg bg-red-500 border-red-500 p-3 border-2 text-white text-center my-2"
+          className="flex-1 rounded-lg bg-purple-500 border-purple-500 p-3 border-2 text-white text-center my-2"
         >
           {copy}
         </button>
